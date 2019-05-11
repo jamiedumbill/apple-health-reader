477829b4e26d468a800137a050ac9e4ee51ed188
Jamie Dumbill <jamie.dumbill@gmail.com>
2019-05-11 11:37:25 -0400

    add handling for file not found

--- src/main/java/io/github/jamiedumbill/applehealthreader/AppleHealthReader.java
+++ src/main/java/io/github/jamiedumbill/applehealthreader/AppleHealthReader.java
@@ -9,6 +9,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Collection;
 
@@ -47,6 +48,8 @@ public class AppleHealthReader {
             }
         } catch (SAXException e) {
             LOGGER.error("SAX error", e);
+        } catch (FileNotFoundException e ){
+            LOGGER.error("Could not find the file {} please check it exists", xmlFilePath);
         } catch (IOException e) {
             LOGGER.error("IO error", e);
         }
